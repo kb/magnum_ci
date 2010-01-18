296c339b8f44c202261382e2cd39cee217f62fd0
Kyle Bolton <kyle.bolton@gmail.com>
2010-01-18 17:57:26 -0500

    Added projects controller, helper, specs;

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ProjectsController do
+
+  #Delete this example and add some real ones
+  it "should use ProjectsController" do
+    controller.should be_an_instance_of(ProjectsController)
+  end
+
+end
