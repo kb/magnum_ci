479ce710c53b4672ec462679bc55648d891ae1cc
Kyle Bolton <kyle.bolton@gmail.com>
2010-01-18 21:07:31 -0500

    Added resource routes for projects; Added custom routing for project show and edit;

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.resources :projects, :except => [:show, :edit]
+  map.root :controller => 'projects'
 
   # See how all your routes lay out with "rake routes"
 
@@ -40,4 +42,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect '/:project', :controller => 'projects', :action => 'show'
+  map.connect '/:project/edit', :controller => 'projects', :action => 'edit'
 end
