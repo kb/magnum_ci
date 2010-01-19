6c016b42347cea3eea2d6cd23d09a0a4a367a31e
Kyle Bolton <kyle.bolton@gmail.com>
2010-01-18 21:58:49 -0500

    Removed public attribute from project and association form;

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,11 +8,11 @@
 #  repo_uri   :string(255)
 #  branch     :string(255)
 #  script     :string(255)
-#  public     :boolean
 #  created_at :datetime
 #  updated_at :datetime
 #
 
 class Project < ActiveRecord::Base
+  validates_presence_of :name, :repo_uri
   validates_uniqueness_of :name
 end
--- db/migrate/20100118221722_create_projects.rb
+++ db/migrate/20100118221722_create_projects.rb
@@ -2,7 +2,6 @@ class CreateProjects < ActiveRecord::Migration
   def self.up
     create_table :projects do |t|
       t.string :name, :repo_uri, :branch, :script
-      t.boolean :public
       t.timestamps
     end
   end
