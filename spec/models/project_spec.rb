c8d7f59838825a32b625fb032cce99db95838339
Kyle Bolton <kyle.bolton@gmail.com>
2010-01-24 23:14:55 -0500

    Gave the spec files some File.dirname love;

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,7 @@
-require 'spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Project do
   before(:each) do
-    @valid_attributes = {
-      
-    }
   end
 
   it "should create a new instance given valid attributes" do
