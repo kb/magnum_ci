1d2e781bf7e7ae3c9dd8f72fea826e7d27118928
Kyle Bolton <kyle.bolton@gmail.com>
2010-01-28 00:06:23 -0500

    Added RedCloth; Instance variables ahoy!

--- lib/magnum_ci/mustache_ride.rb
+++ lib/magnum_ci/mustache_ride.rb
@@ -4,25 +4,30 @@ module MagnumCI
     class << self
       def perform(build_id, project, repo_uri, branch, script)
         @build = Build.find(build_id)
-        MustacheRide.clone(project, repo_uri, branch)
-        MustacheRide.build(script)
+        @project = project
+        @repo_uri = repo_uri
+        @branch = branch
+        @script = script
+        MustacheRide.clone
+        MustacheRide.build
       end
     
       # TODO: Is this possible to use Grit instead of shelling out here?
-      def clone(project, repo_uri, branch)
+      def clone
         # Grab the latest commit bit
-        head = `git ls-remote --heads #{repo_uri} #{branch} | cut -f1`.chomp
+        @head = `git ls-remote --heads #{@repo_uri} #{@branch} | cut -f1`.chomp
       
         # Create a directory and clone the project
         Dir.mkdir "#{RAILS_ROOT}/builds" unless File.directory?("#{RAILS_ROOT}/builds")
-        Dir.mkdir "#{RAILS_ROOT}/builds/#{project}" unless File.directory?("#{RAILS_ROOT}/builds/#{project}")
-        `git clone "#{repo_uri}" "#{RAILS_ROOT}/builds/#{project}/#{head}"`
+        Dir.mkdir "#{RAILS_ROOT}/builds/#{@project}" unless File.directory?("#{RAILS_ROOT}/builds/#{@project}")
+        `git clone "#{@repo_uri}" "#{RAILS_ROOT}/builds/#{@project}/#{@head}"`
       end
     
-      def build(script)
-        stdout = `#{script}`
+      def build
+        stdout = `#{@script}`
         @build.passed = true if $?.to_i == 0
-        @build.log = stdout
+        @build.name = @head
+        @build.log = RedCloth.new(stdout).to_html
         @build.save
       end
     end
