67244f126f363bc21cf88e6c5e842d553f15685f
Kyle Bolton <kyle.bolton@gmail.com>
2010-11-02 15:09:18 -0400

    Tweaked config.yml

--- config/initializers/magnum_ci.rb
+++ config/initializers/magnum_ci.rb
@@ -3,4 +3,4 @@ include Grit
 
 require 'shell'
 
-APP_CONFIG = YAML.load(File.read(Rails.root + "/config/config.yml"))[RAILS_ENV]
+APP_CONFIG = YAML.load(File.read(Rails.root.to_s + '/config/config.yml'))[Rails.env]
