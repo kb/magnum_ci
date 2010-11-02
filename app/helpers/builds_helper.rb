801b356a57a7c411bcb14b114b21b48dc384cf5b
Kyle Bolton <kyle.bolton@gmail.com>
2010-11-01 23:26:43 -0400

    Added raw method call to log output. (Still looking into a better way to display the log)

--- app/helpers/builds_helper.rb
+++ app/helpers/builds_helper.rb
@@ -13,7 +13,7 @@ module BuildsHelper
 
   def pretty_print_log(log)
     return unless log
-    log.gsub("\e[0m", '</span>').
+    raw(log.gsub("\e[0m", '</span>').
             gsub("\e[31m", '<span class="color31">').
             gsub("\e[32m", '<span class="color32">').
             gsub("\e[33m", '<span class="color33">').
@@ -21,6 +21,6 @@ module BuildsHelper
             gsub("\e[35m", '<span class="color35">').
             gsub("\e[36m", '<span class="color36">').
             gsub("\e[37m", '<span class="color37">').
-            gsub(/\n/, '<br>')
+            gsub(/\n/, '<br>'))
   end
 end
